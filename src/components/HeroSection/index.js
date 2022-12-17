3fb44dfb9a14ef91836eb6a37b30baed240acc46
Hilarius Laru Doren <larudoren@users.noreply.github.com>
2022-12-17 20:56:47 +0700

    Add files via upload

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import {  HeroContainer, HeroBg } from './HeroElements';
+import React, { useState } from 'react';
+import { Button } from '../ButtonElement';
+import {  
+    HeroContainer, 
+    HeroContent, 
+    HeroH1, 
+    HeroP, 
+    HeroBtnWrapper, 
+    ArrowFoward, 
+    ArrowRight,  
+    HeroBg
+} from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer>
-            {/* <HeroBg>
+            <HeroBg>
 
-            </HeroBg> */}
-            <HerContent>
-                <HeroH1>
-                    
-                </HeroH1>
-            </HerContent>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>The most agile ERP on the planet</HeroH1>
+                <HeroP>
+                    ERPNext is the world's best free and open source ERP
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="/" onMouseEnter={onHover} onMouseLeave={onHover}
+                    primary="true" dark="true"
+                    >
+                        Get started {hover ? <ArrowFoward /> : <ArrowRight />}</Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }

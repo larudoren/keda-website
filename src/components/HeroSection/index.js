83ef235271da219143cb595f52e80d846427ae55
Hilarius Laru Doren <larudoren@users.noreply.github.com>
2022-12-17 19:18:11 +0700

    Add files via upload

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {  HeroContainer, HeroBg } from './HeroElements';
+
+const HeroSection = () => {
+    return (
+        <HeroContainer>
+            {/* <HeroBg>
+
+            </HeroBg> */}
+            <HerContent>
+                <HeroH1>
+                    
+                </HeroH1>
+            </HerContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection;
